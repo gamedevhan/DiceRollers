8fbaf586effc38eccb75a65a746104cc515c1641
Han Jung <gamedevhan@gmail.com>
2018-05-14 22:18:14 -0400

    Add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{	
+	public Transform currentCharacter;
+	
+	private Vector3 offset;
+
+	private void Start()
+	{
+		offset = transform.position - currentCharacter.position;
+	}
+
+	private void LateUpdate()
+	{
+		transform.position = currentCharacter.position + offset;
+	}
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,7 +6,12 @@ public class Tile : MonoBehaviour
 
 	// This event needs to be raised when character finish lerping
 	public delegate void EnterTileEvent();
-	public static event EnterTileEvent TileEntered;	
+	public static event EnterTileEvent TileEntered;
+
+	private void Start()
+	{
+		
+	}
 
 	public void OnCharacterEnter()
 	{
