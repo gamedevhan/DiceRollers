db46825a5dc77e43dcbb4f289d4439368c43d983
Han Jung <gamedevhan@gmail.com>
2018-05-30 22:29:54 -0400

    Organize folder

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : MonoBehaviour
+{
+	private void OnEnable()
+	{
+		
+	}
+
+	private void OnDisable()
+	{
+		
+	}
+}
