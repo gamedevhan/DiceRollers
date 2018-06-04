29d1944c6421a9d7f80c0db6407a87886a480734
Han Jung <gamedevhan@gmail.com>
2018-06-04 18:15:52 -0400

    Room scene changes
    - Seperate RoomPlayerUI and Displayer
    - RoomPlayer owned by scene set inactive
    - Send RPC to sync user's current character

---
+++ Assets/Scripts/Room/CharacterDisplayer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDisplayer : MonoBehaviour
+{	
+	[SerializeField]
+	private Transform charactersParent;
+
+	private List<GameObject> characters = new List<GameObject>();
+	private int currentCharacterIndex = 0;
+	
+	private void Awake()
+	{
+		foreach (Transform character in charactersParent)
+		{			
+			characters.Add(character.gameObject);		
+		}
+	}
+
+	private void Start()
+	{
+		FaceCamera();
+		HideSceneOwned();
+	}
+	
+	[PunRPC]
+	public void DisplayPreviousCharacter()
+	{
+		// Get character index
+		currentCharacterIndex = currentCharacterIndex != 0 ? currentCharacterIndex - 1 : characters.Count - 1;
+		
+		DisplayCurrentCharacter();
+	}
+
+	[PunRPC]
+	public void DisplayNextCharacter()
+	{
+		// Get character index
+		currentCharacterIndex = currentCharacterIndex != characters.Count - 1 ? currentCharacterIndex + 1 : 0;
+
+		DisplayCurrentCharacter();
+	}
+
+	private void HideSceneOwned()
+	{		
+		PhotonView photonView = PhotonView.Get(this);
+
+		if (photonView.isSceneView)
+		{
+			charactersParent.gameObject.SetActive(false);
+		}
+	}
+		
+	private void DisplayCurrentCharacter()
+	{
+		foreach (Transform characterTransform in charactersParent)
+		{
+			characterTransform.gameObject.SetActive(false);
+		}		
+		characters[currentCharacterIndex].SetActive(true);
+	}
+
+	private void FaceCamera()
+	{
+		Camera camera = Camera.main;
+		Vector3 targetPosition = new Vector3(camera.transform.position.x, transform.position.y, camera.transform.position.z);
+		charactersParent.LookAt(targetPosition);
+	}
+}
---
+++ Assets/Scripts/Room/RoomPlayerUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class RoomPlayerUI : MonoBehaviour
+{		
+	public UILabel PlayerName;
+	public UISprite ReadySprite;
+	public bool IsReady = false;
+	
+	[SerializeField]
+	private GameObject uiButtons;
+
+	private PhotonView photonView;
+		
+	private void Awake()
+	{	
+		photonView = PhotonView.Get(this);
+		
+		HideNotMine();
+	}
+
+	private void HideNotMine()
+	{
+		if (!photonView.isMine || photonView.isSceneView)
+		{ uiButtons.SetActive(false); }
+
+		if (photonView.isSceneView)
+		{
+			PlayerName.alpha = 0;
+		}
+	}
+
+	public void OnPreviousPressed()
+	{
+		photonView.RPC("DisplayPreviousCharacter", PhotonTargets.All);
+	}
+
+	public void OnNextPressed()
+	{
+		photonView.RPC("DisplayNextCharacter", PhotonTargets.All);
+	}
+}
