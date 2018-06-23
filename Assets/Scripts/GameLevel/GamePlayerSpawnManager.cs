1e9cf91d9e383e546f154f87c2c2b23fe603de0b
Han Jung <gamedevhan@gmail.com>
2018-06-22 22:51:39 -0400

    Update game level character settings

---
+++ Assets/Scripts/GameLevel/GamePlayerSpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GamePlayerSpawnManager : MonoBehaviour
+{
+	[SerializeField]
+	private Transform startTile;
+
+	// Use this for initialization
+	void Start()
+	{
+		SpawnGamePlayer();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void SpawnGamePlayer()
+	{		
+		string characterName = LevelTransitionManager.Instance.SelectedCharacter.ToString();
+		Debug.Log(characterName);
+		//PhotonNetwork.Instantiate(characterName, startTile.position, Quaternion.identity, 0);
+	}
+}
