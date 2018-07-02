2dbce11cedf67a03bc8ca9ac280c2c500b6f1f07
Han Jung <gamedevhan@gmail.com>
2018-07-02 15:14:15 -0400

    Add custom Debug class

---
+++ Assets/Scripts/Utility/DebugColor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class DebugColor
+{
+	public static void Log(string text, string color = "green")
+    {
+        Debug.Log("<b><color=" + color + ">" + text + "</color></b>");
+    }
+}
