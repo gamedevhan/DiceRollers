1edb344766c3428ee6a7c3484b09e75eb7495841
Han Jung <gamedevhan@gmail.com>
2018-07-08 22:08:32 -0400

    Use NGUI text, fix build error

--- Assets/Scripts/GameLevel/Dice.cs
+++ Assets/Scripts/GameLevel/Dice.cs
@@ -32,7 +32,7 @@ public class Dice : MonoBehaviour
 		DebugUtility.Log(DiceResult);
 		photonView.RPC("PlayRollAnimation", PhotonTargets.All, DiceResult);
 
-		yield return new WaitForSeconds(2f);
+		yield return new WaitForSeconds(1f);
 		DiceRollEvent(DiceResult);
         yield return new WaitForSeconds(1f);
         MeshRednderer.enabled = false;
