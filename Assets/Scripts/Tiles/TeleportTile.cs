e8c9d402c140d6b8d1928278d4fa65205a3131b0
Han Jung <gamedevhan@gmail.com>
2018-07-11 21:19:22 -0400

    Fix remaining issues and clean up code
    - Move redudndant logic to Tile class
    - StartCoroutine instead of calling coroutine methods

--- Assets/Scripts/Tiles/TeleportTile.cs
+++ Assets/Scripts/Tiles/TeleportTile.cs
@@ -19,42 +19,7 @@ public class TeleportTile : Tile, ISpecialTile
 
     public override void OnCharacterEnter(CharacterMovementController character)
     {
-        if (character.MoveLeft > 0) // character is moving forward
-        {
-            // Are we on the last tile?
-            if (isEndTile)
-            {
-                GameManager.Instance.GameOver();
-            }
-            else
-            {
-                character.CurrentTile = TileManager.Instance.Tiles[index];
-                character.NextTile = TileManager.Instance.Tiles[index + 1];
-                StartCoroutine(character.Move());
-            }
-        }
-        else if (character.MoveLeft < 0) // character is moving forward
-        {
-            // Are we on the first tile?
-            if (isStartTile)
-            {
-                character.MoveLeft = 0;
-                GameManager.Instance.TurnManager.TurnEnd(); // Moved backward and reached start tile, end turn
-            }
-            else
-            {
-                character.CurrentTile = TileManager.Instance.Tiles[index];
-                character.NextTile = TileManager.Instance.Tiles[index - 1];
-                StartCoroutine(character.Move());
-            }
-        }
-        else // if (character.MoveLeft == 0)
-        {
-            character.CurrentTile = TileManager.Instance.Tiles[index];
-            character.NextTile = TileManager.Instance.Tiles[index + 1];
-            character.ShouldPlayMoveAnim = false;
-            SpecialTileBehaviour(character);
-        }    
+        base.OnCharacterEnter(character);        
     }
 
     public IEnumerator SpecialTileBehaviour(CharacterMovementController character)
--- Assets/Scripts/Tiles/Tile.cs
+++ Assets/Scripts/Tiles/Tile.cs
@@ -19,5 +19,52 @@ public abstract class Tile : MonoBehaviour
         }
     }
 
-    public abstract void OnCharacterEnter(CharacterMovementController character);
+    public virtual void OnCharacterEnter(CharacterMovementController character)
+	{
+		if (character.MoveLeft > 0) // character is moving forward
+        {
+            // Are we on the last tile?
+            if (isEndTile)
+            {
+                GameManager.Instance.GameOver();
+            }
+            else
+            {
+                character.CurrentTile = TileManager.Instance.Tiles[index];
+                character.NextTile = TileManager.Instance.Tiles[index + 1];
+                StartCoroutine(character.Move());
+            }
+        }
+        else if (character.MoveLeft < 0) // character is moving backward
+        {
+            // Are we on the first tile?
+            if (isStartTile)
+            {
+                character.MoveLeft = 0;
+                GameManager.Instance.TurnManager.TurnEnd(); // Moved backward and reached start tile, end turn
+            }
+            else
+            {
+                character.CurrentTile = TileManager.Instance.Tiles[index];
+                character.NextTile = TileManager.Instance.Tiles[index - 1];
+                StartCoroutine(character.Move());
+            }
+        }
+        else // if (character.MoveLeft == 0)
+        {
+            character.CurrentTile = TileManager.Instance.Tiles[index];
+            character.NextTile = TileManager.Instance.Tiles[index + 1];
+            character.ShouldPlayMoveAnim = false;
+
+			ISpecialTile specialTile = GetComponent<ISpecialTile>();
+			if (specialTile != null)
+			{
+				StartCoroutine(specialTile.SpecialTileBehaviour(character));
+			}
+			else
+			{
+				GameManager.Instance.TurnManager.TurnEnd();
+			}
+        }
+	}
 }
