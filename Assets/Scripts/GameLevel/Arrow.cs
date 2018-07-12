96c16374bfaee64e61870c0c693050652ed167f3
Han Jung <gamedevhan@gmail.com>
2018-07-12 19:52:29 -0400

    Add Intersection tile and arrow

---
+++ Assets/Scripts/GameLevel/Arrow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+	public MeshRenderer MeshRenderer { get; private set; }
+
+	private void Awake()
+	{
+		MeshRenderer = GetComponentInChildren<MeshRenderer>();
+		MeshRenderer.enabled = false;
+	}
+
+	public void LookatTarget(Vector3 targetPosition)
+	{
+		transform.LookAt(targetPosition);
+	}
+
+	public Arrow OnArrowPressed()
+	{
+		StartCoroutine(Blink());
+		return this;
+	}
+
+	public IEnumerator Blink()
+	{		
+		MeshRenderer.enabled = false;
+		yield return new WaitForSeconds(0.25f);
+		MeshRenderer.enabled = true;
+		yield return new WaitForSeconds(0.25f);
+		MeshRenderer.enabled = false;
+		yield return new WaitForSeconds(0.25f);
+	}
+}
--- Assets/Scripts/Tiles/IntersectionTile.cs
+++ Assets/Scripts/Tiles/IntersectionTile.cs
@@ -1,12 +1,36 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class IntersectionTile : Tile
 {
-	[SerializeField]	
-	private GameObject[] arrows;	
+	[SerializeField]
+	private GameObject arrowPrefab;	
+	private List<Tile> nextTiles = new List<Tile>();
+	private Dictionary<Arrow, Tile> direction = new Dictionary<Arrow, Tile>();
+
+	protected override void Start()
+	{
+		base.Start();
+		for (int i = 1; i < neighbourTiles.Count; i++)
+		{
+			nextTiles.Add(neighbourTiles[i]);
+			GameObject arrowGameObject = Instantiate(arrowPrefab, transform);
+			Arrow arrow = arrowGameObject.GetComponent<Arrow>();
+			direction.Add(arrow, neighbourTiles[i]);
+
+			Vector3 arrowPosition = new Vector3((neighbourTiles[i].transform.position.x + transform.position.x) / 2, 0, (neighbourTiles[i].transform.position.z + transform.position.z) / 2);
+			arrow.transform.position = arrowPosition;
+
+			Vector3 targetPosition = new Vector3(neighbourTiles[i].transform.position.x, transform.position.y, neighbourTiles[i].transform.position.z);
+			arrow.LookatTarget(targetPosition);
+		}
+	}
 
 	public override void OnCharacterEnter(CharacterMovementController character)
 	{
-		
+		foreach (Arrow arrow in direction.Keys)
+		{
+			arrow.MeshRenderer.enabled = true;
+		}
 	}
 }
