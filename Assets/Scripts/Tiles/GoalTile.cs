31f6d59ec4d073f5c3b6bbe0e1172f52f496b780
Han Jung <Han@getluckyvr.com>
2018-07-23 08:37:08 -0400

    Add fireworks launching on goal tile reached

--- Assets/Scripts/Tiles/GoalTile.cs
+++ Assets/Scripts/Tiles/GoalTile.cs
@@ -1,16 +1,54 @@
-﻿public class GoalTile : Tile
-{
-	protected override void Start()
-	{
-		base.Start();
-	}
+﻿using System.Collections;
+using UnityEngine;
 
-	public override void OnCharacterEnter(CharacterMovementController character)
-	{
-		character.MoveLeft = 0;
+public class GoalTile : Tile
+{
+    #region FireWorksTest
+    public bool Test;
+    public bool instantiated = false;
+    private void Update()
+    {
+        if (Test && instantiated)
+        {
+            instantiated = false;
+            LaunchFireworks();
+            StartCoroutine(LaunchFireworks());
+        }
+    }
+    #endregion
+
+    [SerializeField]
+    private GameObject[] fireWorks;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override void OnCharacterEnter(CharacterMovementController character)
+    {
+        character.MoveLeft = 0;
         character.GetComponent<CharacterAnimationController>().PlayWalkAnimation(false);
-		
-		string ownerNickName = character.PhotonView.owner.NickName;
-		GameManager.Instance.GameOver(ownerNickName);		
-	}
+
+        GetComponent<PhotonView>().RPC("RpcInstantiateFireworks", PhotonTargets.All);
+
+        string ownerNickName = character.PhotonView.owner.NickName;
+        GameManager.Instance.GameOver(ownerNickName);
+    }
+
+    [PunRPC]
+    private void RpcInstantiateFireworks()
+    {
+        StartCoroutine(LaunchFireworks());
+    }
+
+    private IEnumerator LaunchFireworks()
+    {
+        for (int i = 0; i < fireWorks.Length; i++)
+        {
+            Vector3 position = new Vector3(0, transform.position.y + 3, 0);
+            Instantiate(fireWorks[i], position, Quaternion.identity);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }   
 }
