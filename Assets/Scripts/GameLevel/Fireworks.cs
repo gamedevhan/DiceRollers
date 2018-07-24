810bcfb29a2f8631e360784e0d5b76f116180c5f
Han Jung <Han@getluckyvr.com>
2018-07-24 07:54:54 -0400

    Add fireworks class, separate from goal tile
    single responsibility principle

---
+++ Assets/Scripts/GameLevel/Fireworks.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Fireworks : MonoBehaviour
+{
+    #region FireWorksTest
+    public bool Test;
+    public bool instantiated = false;
+    private void Update()
+    {
+        if (Test && instantiated)
+        {
+            instantiated = false;
+            LaunchFireworks();
+            StartCoroutine(LaunchFireworks());
+        }
+    }
+    #endregion
+    
+    [SerializeField]
+    private GameObject[] fireWorks;
+
+    [PunRPC]
+    private void RpcInstantiateFireworks()
+    {
+        StartCoroutine(LaunchFireworks());
+    }
+
+    private IEnumerator LaunchFireworks()
+    {
+        for (int i = 0; i < fireWorks.Length; i++)
+        {
+            Vector3 position = new Vector3(0, transform.position.y + 3, 0);
+            Instantiate(fireWorks[i], position, Quaternion.identity);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
--- Assets/Scripts/Tiles/GoalTile.cs
+++ Assets/Scripts/Tiles/GoalTile.cs
@@ -3,23 +3,6 @@ using UnityEngine;
 
 public class GoalTile : Tile
 {
-    #region FireWorksTest
-    public bool Test;
-    public bool instantiated = false;
-    private void Update()
-    {
-        if (Test && instantiated)
-        {
-            instantiated = false;
-            LaunchFireworks();
-            StartCoroutine(LaunchFireworks());
-        }
-    }
-    #endregion
-
-    [SerializeField]
-    private GameObject[] fireWorks;
-
     protected override void Start()
     {
         base.Start();
@@ -35,20 +18,4 @@ public class GoalTile : Tile
         string ownerNickName = character.PhotonView.owner.NickName;
         GameManager.Instance.GameOver(ownerNickName);
     }
-
-    [PunRPC]
-    private void RpcInstantiateFireworks()
-    {
-        StartCoroutine(LaunchFireworks());
-    }
-
-    private IEnumerator LaunchFireworks()
-    {
-        for (int i = 0; i < fireWorks.Length; i++)
-        {
-            Vector3 position = new Vector3(0, transform.position.y + 3, 0);
-            Instantiate(fireWorks[i], position, Quaternion.identity);
-            yield return new WaitForSeconds(0.5f);
-        }
-    }   
 }
